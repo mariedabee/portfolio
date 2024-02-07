bd0f7e7002552e5d5d1901a02c5ea76be958d68c
mariedabee <mariejuly70@gmail.com>
2024-02-07 22:17:08 +0100

    creating a navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AppBar, Toolbar, Typography, Button } from '@mui/material';
+
+const Navbar: React.FC = () => {
+    return (
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    My Portfolio
+                </Typography>
+                <Button color="inherit">Home</Button>
+                <Button color="inherit">About</Button>
+                <Button color="inherit">Portfolio</Button>
+                <Button color="inherit">Contact</Button>
+            </Toolbar>
+        </AppBar>
+    );
+}
+
+export default Navbar;
