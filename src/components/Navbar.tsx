80d313463530f6d54b373bf130433d9ad4b2aaf3
mariedabee <mariejuly70@gmail.com>
2024-02-07 23:18:30 +0100

    navbar styled

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,34 @@
 // src/components/Navbar.tsx
 
 import React from 'react';
-import { AppBar, Toolbar, Typography, Button } from '@mui/material'; 
+import { AppBar, Toolbar, Button } from '@mui/material'; 
 import logo from '../shared/logo.png';
+import './Navbar.css';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+// Define a custom theme with the desired AppBar color
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#fff2cc', 
+        },
+    },
+});
 
 const Navbar: React.FC = () => {
     return (
-        <AppBar position="static">
+    <ThemeProvider theme={theme}>
+        <AppBar  position="static">
             <Toolbar>
-                <img src={logo} alt="logo" style={{ height: '50px', marginRight: '10px' }} /> 
+                <img src={logo} alt="logo" className="logo" /> 
                 <Button color="inherit">Home</Button>
                 <Button color="inherit">About</Button>
                 <Button color="inherit">Portfolio</Button>
                 <Button color="inherit">Contact</Button>
             </Toolbar>
         </AppBar>
+    </ThemeProvider>
+
     );
 }
 
