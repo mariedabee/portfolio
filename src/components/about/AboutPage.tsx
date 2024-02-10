f4132bbfdd6c1e1038ae3d523a38f053a55c5291
mariedabee <mariejuly70@gmail.com>
2024-02-10 19:03:12 +0100

    about page restyled with image

--- src/components/about/AboutPage.tsx
+++ src/components/about/AboutPage.tsx
@@ -1,57 +1,66 @@
 import React from 'react';
-import { Box, Card, Container, Typography } from '@mui/material';
+import { Box, Card, CardMedia, Container, Typography } from '@mui/material';
 import { useStyles } from '../styles';
+import naturePortfolio from '../../shared/nature_portfolio.jpg';
 
 const AboutPage: React.FC = () => {
     const classes = useStyles();
 
     return (
-    <Box className={classes.root}>
+        <Container>
+            <Box className={classes.root}>
+                {/* Text content */}
+            <Card className={`${classes.card} ${classes.textCard}`}>                    <Typography variant="h2" align="center" gutterBottom className={classes.vintageText}>
+                        About Me
+                    </Typography>
+                    {/* Skills & Experiences */}
+                    <Typography variant="h4" align="center" gutterBottom className={classes.vintageText} >
+                        Skills & Experiences
+                    </Typography>
+                    <Typography variant="body1" align="left" paragraph className={classes.vintageText} >
+                        Proficient in Java, HTML, CSS, and beginner in React.
+                    </Typography>
+                    <Typography variant="body1" align="left" paragraph className={classes.vintageText}>
+                        Experienced in marketing and technicalities of advertisement placement.
+                    </Typography>
+                    <Typography variant="body1" align="left" paragraph className={classes.vintageText}>
+                        Previous work experience in IT at Encoway in Bremen, gaining insights into real-world development environments and sharpening programming skills.
+                    </Typography>
 
-        <Card className={classes.card}>
-            <Typography variant="h2" align="center" gutterBottom className={classes.vintageText}>
-                About Me
-            </Typography>
-        </Card>
-        <Card className={classes.card}>
-            <Typography variant="h4" align="center" gutterBottom className={classes.vintageText} >
-                Skills & Experiences
-            </Typography>
-            <Typography variant="body1" align="left" paragraph className={classes.vintageText} >
-                Proficient in Java, HTML, CSS, and beginner in React.
-            </Typography>
-            <Typography variant="body1" align="left" paragraph className={classes.vintageText}>
-                Experienced in marketing and technicalities of advertisement placement.
-            </Typography>
-            <Typography variant="body1" align="left" paragraph className={classes.vintageText}>
-            Previous work experience in IT at Encoway in Bremen, gaining insights into real-world development environments and sharpening programming skills.
-            </Typography>
-        </Card>
+                    {/* Hobbies & Interests */}
+                    <Typography variant="h4" align="center" gutterBottom className={classes.vintageText}>
+                        Hobbies & Interests
+                    </Typography>
+                    <Typography variant="body1" align="left" paragraph className={classes.vintageText}>
+                        Enjoys reading, writing, painting, kickboxing, and discovering new music.
+                    </Typography>
+                    <Typography variant="body1" align="left" paragraph className={classes.vintageText}>
+                        Fascinated by AI and aspiring to make meaningful contributions to humanity with software-related skills and knowledge.
+                    </Typography>
 
-        <Card className={classes.card}>
-        <Typography variant="h4" align="center" gutterBottom className={classes.vintageText}>
-          Hobbies & Interests
-           </Typography>
-           <Typography variant="body1" align="left" paragraph className={classes.vintageText}>
-            Enjoys reading, writing, painting, kickboxing, and discovering new music.
-           </Typography>
-           <Typography variant="body1" align="left" paragraph className={classes.vintageText}>
-            Fascinated by AI and aspiring to make meaningful contributions to humanity with software-related skills and knowledge.
-           </Typography>
-           </Card>
+                    {/* Aspirations */}
+                    <Typography variant="h4" align="center" gutterBottom className={classes.vintageText}>
+                        Aspirations
+                    </Typography>
+                    <Typography variant="body1" align="left" paragraph className={classes.vintageText}>
+                        Seeking a position as a software developer in a supportive environment where I can continue to learn and grow.
+                    </Typography>
+                    <Typography variant="body1" align="left" paragraph className={classes.vintageText}>
+                        Eager to travel and explore, and to eventually use my skills to do something helpful for humanity.
+                    </Typography>
+                </Card>
 
-           <Card className={classes.card}>
-           <Typography variant="h4" align="center" gutterBottom className={classes.vintageText}>
-           Aspirations
-           </Typography>
-           <Typography variant="body1" align="left" paragraph className={classes.vintageText}>
-            Seeking a position as a software developer in a supportive environment where I can continue to learn and grow.
-           </Typography>
-           <Typography variant="body1" align="left" paragraph className={classes.vintageText}>
-            Eager to travel and explore, and to eventually use my skills to do something helpful for humanity.
-           </Typography>
-            </Card>
-        </Box>
+                {/* Image */}
+            <Card className={`${classes.card} ${classes.imageCard}`}>     
+                           <CardMedia className={classes.imageCard}
+                        component="img"
+                        src={naturePortfolio} // Pass the imported image variable
+                        alt="Your Image Alt Text"
+                        style={{ maxWidth: '100%', height: 'auto' }}
+                    />
+                </Card>
+            </Box>
+        </Container>
     );
 }
 
--- src/components/styles.ts
+++ src/components/styles.ts
@@ -6,13 +6,10 @@ const vintageFont = "'Old Standard TT', serif";
 
 export const useStyles = makeStyles((theme: any) => ({
   root: {
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-    backdropFilter: "blur(57px)", // Reduced blur effect
-    padding: "50px",
-    borderRadius: "10px",
-    margin: 0,
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "stretch",
+    flexWrap: "wrap", // Allow wrapping for smaller screens
   },
   card: {
     transition: "transform 0.3s ease",
@@ -21,10 +18,19 @@ export const useStyles = makeStyles((theme: any) => ({
     },
     marginBottom: theme.spacing(2),
     fontFamily: vintageFont,
-    backgroundColor: "rgba(255, 255, 255, 0.7)",
+    backgroundColor: "rgba(255, 255, 255, 0.5)",
+    flex: 1, // Make cards grow to fill available space
+    flexBasis: "100%",
   },
   vintageText: {
     fontFamily: vintageFont,
     color: "#12372A",
+  },
+  textCard: {
+    marginRight: theme.spacing(2), // Adjust margin to create space between cards
+    flexBasis: "60%", // Set the width of the text card
+  },
+  imageCard: {
+    flexBasis: "30%", // Set the width of the image card
   },
 }));
