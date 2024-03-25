7f21987ca0b9a9851d56769fd73a7cf288fc651e
mariedabee <mariejuly70@gmail.com>
2024-03-25 17:46:57 +0100

    sign up works

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 //server.js
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const { submitForm } = require("./routes/formSubmitController");
-require("dotenv").config();
+const { signup } = require("./routes/signupController");
+// Import MongoDB connection middleware
+const { connectToMongoDB } = require("./mongoDB");
+const postsRouter = require("./routes/postsRouter");
 
 const app = express();
 
@@ -13,12 +17,6 @@ app.use(cors());
 // Parse JSON bodies of requests
 app.use(bodyParser.json());
 
-// Import MongoDB connection middleware
-const { connectToMongoDB } = require("./mongoDB");
-
-// Import routes
-const postsRouter = require("./routes/postsRouter");
-const authRouter = require("./routes/authRouter"); // Import authRouter
 
 // Connect to MongoDB and start the server
 connectToMongoDB()
@@ -31,10 +29,12 @@ connectToMongoDB()
 
     // Use routes
     app.use("/api/posts", postsRouter); // Attach postsRouter for /api/posts routes
-    app.use("/api/auth", authRouter); // Attach authRouter for /api/auth routes
 
     // Endpoint to handle form submission
     app.post("/submit-form", submitForm);
+
+    // Endpoint to handle user signup
+    app.post("/signup", signup);
 
     // Start the server
     const PORT = process.env.PORT || 3001;
